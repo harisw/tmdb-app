0d354e3ce9692720d4913cd03dc00f56ef18d4ad
Hari Setiawan <harrysw6@gmail.com>
2025-05-27 23:50:40 +0900

    chore: add LanguageSeeder

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -9,4 +9,10 @@ class Language extends Model
 {
     /** @use HasFactory<\Database\Factories\LanguageFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'code'
+    ];
 }
--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
+use Exception;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,29 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $file = database_path('data/languages.csv');
+
+        if (!file_exists($file) || !is_readable($file)) {
+            throw new Exception("File not found");
+        }
+
+        $header = null;
+        $data = [];
+
+        if (($handle = fopen($file, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 1000, ',')) !== false) {
+                if (!$header) {
+                    $header = $row;
+                } else {
+                    $data[] = array_combine($header, $row);
+                }
+            }
+            fclose($handle);
+        }
+
+
+        foreach (array_chunk($data, 1000) as $chunkedData) {
+            Language::upsert($chunkedData, ['code']);
+        }
     }
 }
