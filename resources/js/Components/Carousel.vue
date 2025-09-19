76f528a5673fee398c8fee36a759a3909d24c4c1
Hari <harrysw6@gmail.com>
2025-09-19 18:03:05 +0900

    feat: add allGenre page

---
+++ resources/js/Components/Carousel.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import {ArrowBigLeft, ArrowBigRight} from "lucide-vue-next";
+import {ref} from "vue";
+
+const carousel = ref(null);
+const scrollLeft = () => {
+    carousel.value.scrollBy({left: -150, behavior: "smooth"})
+}
+
+const scrollRight = () => {
+    carousel.value.scrollBy({left: 400, behavior: "smooth"})
+}
+</script>
+
+<template>
+    <div class="relative w-full">
+        <div
+            ref="carousel"
+            class="flex space-x-8 gap-10 overflow-x-auto scroll-smooth no-scrollbar"
+        >
+            <slot/>
+
+
+        </div>
+        <!-- Controls -->
+        <button
+            @click="scrollLeft"
+            class="cursor-pointer absolute left-0 top-1/2 -translate-x-5/6 -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70"
+        >
+            <ArrowBigLeft size="46"/>
+        </button>
+        <button
+            @click="scrollRight"
+            class="cursor-pointer absolute right-0 top-1/2 translate-x-5/6 -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70"
+        >
+            <ArrowBigRight size="46"/>
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/AllGenres.vue
+++ resources/js/Pages/AllGenres.vue
@@ -1,63 +1,10 @@
-<template>
-    <Head title="Home"/>
-    <AppLayout>
-        <div>
-            <h1 class="text-2xl font-bold mb-6 text-madder">{{ allGenres[0].name }}</h1>
-            <div
-                ref="carousel"
-                class="flex space-x-8 gap-10 overflow-x-auto scroll-smooth no-scrollbar"
-            >
-                <div v-for="item in allGenres[0].movies" :key="item.id" class="bg-white rounded shadow ">
-                    <div class="w-3xs group [perspective:500px] mx-auto h-full hover:cursor-pointer"
-                         @click="openModal(item)">
-                        <div class="bg-white rounded-xl shadow-xl transition-transform duration-500 transform group-hover:rotate-y-3
-                        group-hover:-rotate-x-1 group-hover:scale-105 [transform-style:preserve-3d] overflow-hidden h-full">
-
-                            <img :src="poster_url + item.img_poster" alt="Movie Poster"
-                                 class="w-full h-90 object-cover rounded-t-xl"/>
-
-                            <div class="p-2">
-                                <div class="flex flex-row justify-between">
-                                    <h2 class="text-lg font-bold text-gray-800">{{ item.title }}</h2>
-                                    <RatingBadge :rating="item.rating" size="xs"/>
-                                </div>
-                                <!--                                <div class="flex flex-row gap-2 text-xs">-->
-                                <!--                                    <span v-for="genre in item.genres.slice(0, 3)" :key="genre.id" class="py-0.5 px-1-->
-                                <!--                                     font-semibold bg-tea-green rounded">-->
-                                <!--                                        {{ genre.name }}-->
-                                <!--                                    </span>-->
-                                <!--                                </div>-->
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-                <!-- Controls -->
-                <button
-                    @click="scrollLeft"
-                    class="absolute left-0 top-1/2 -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70"
-                >
-                    ‹
-                </button>
-                <button
-                    @click="scrollRight"
-                    class="absolute right-0 top-1/2 -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70"
-                >
-                    ›
-                </button>
-            </div>
-        </div>
-    </AppLayout>
-    <MovieModal :show="showModal" :item="selectedItem" @close="closeModal" :backdrop_url="backdrop_url"/>
-</template>
-
 <script setup>
 import {Head} from "@inertiajs/vue3";
-import {ref, onMounted, onUnmounted} from "vue";
+import {ref} from "vue";
 import AppLayout from "../Layouts/AppLayout.vue";
 import MovieModal from "../Components/Movie-Modal.vue"
 import RatingBadge from "../Components/RatingBadge.vue";
+import Carousel from "../Components/Carousel.vue";
 
 defineProps({
     allGenres: Array,
@@ -78,50 +25,44 @@ function closeModal() {
     showModal.value = false;
 }
 
-const carousel = ref(null);
-let autoplayTimer = null;
 
-const scrollLeft = () => {
-    carousel.value.scrollBy({left: -150, behavior: "smooth"})
-}
-
-const scrollRight = () => {
-    carousel.value.scrollBy({left: 400, behavior: "smooth"})
-}
-
-const startAutoplay = () => {
-    stopAutoplay();
-    autoplayTimer = setInterval(() => {
-        if (!carousel.value) return;
-        const maxScroll = carousel.value.scrollWidth - carousel.value.clientWidth;
-
-        if (carousel.value.scrollLeft >= maxScroll - 10) {
-            carousel.value.scrollTo({left: 0, behavior: "smooth"});
-        } else {
-            scrollRight();
-        }
-    }, 5);
-};
-
-const stopAutoplay = () => {
-    if (autoplayTimer) {
-        clearInterval(autoplayTimer);
-        autoplayTimer = null;
-    }
-}
-
-onMounted(() => {
-    startAutoplay();
-
-    // Pause autoplay when mouse enters, resume when leaves
-    carousel.value.addEventListener("mouseenter", stopAutoplay);
-    carousel.value.addEventListener("mouseleave", startAutoplay);
-});
-
-onUnmounted(() => {
-    stopAutoplay();
-});
 </script>
+
+<template>
+    <Head title="Home"/>
+    <AppLayout>
+        <div class="w-full">
+            <div v-for="genre in allGenres" :key="genre.id" class="mb-15">
+                <h1 class="text-2xl font-bold mb-6 text-madder">{{ genre.name }}</h1>
+                <Carousel>
+                    <div v-for="item in genre.movies" :key="item.id" class="bg-white rounded shadow ">
+                        <div
+                            class="w-3xs group [perspective:500px] mx-auto h-full hover:cursor-pointer overflow-hidden"
+                            @click="openModal(item)">
+                            <div class="bg-white rounded-xl shadow-xl transition-transform duration-500 transform group-hover:rotate-y-3
+                        group-hover:-rotate-x-1 group-hover:scale-105 [transform-style:preserve-3d] overflow-hidden h-full">
+
+                                <img :src="poster_url + item.img_poster" alt="Movie Poster"
+                                     class="w-full h-90 object-cover rounded-t-xl"/>
+
+                                <div class="p-2">
+                                    <div class="flex flex-row justify-between">
+                                        <h2 class="text-lg font-bold text-gray-800">{{ item.title }}</h2>
+                                        <RatingBadge :rating="item.rating" size="xs"/>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </Carousel>
+            </div>
+        </div>
+    </AppLayout>
+    <MovieModal :show="showModal" :item="selectedItem" @close="closeModal" :backdrop_url="backdrop_url"/>
+</template>
 
 <style>
 .no-scrollbar::-webkit-scrollbar {
