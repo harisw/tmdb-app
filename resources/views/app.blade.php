81d6c376282d69fbbe4090e063de1f8cc86069ea
Hari <harrysw6@gmail.com>
2025-09-25 09:35:33 +0900

    chore: add favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
     @vite('resources/js/app.js')
 </head>
 <body>
